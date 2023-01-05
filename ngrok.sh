e33dcc7410c4176f8c131f31683d091d41fad102
ucox777 <85094433+ucox777@users.noreply.github.com>
2023-01-06 04:35:06 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -4,7 +4,7 @@ echo "======================="
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
-./ng.sh
+./ng.sh config add-authtoken 2JuOmpSKRCXYdDaOnBOFiuUFN9j_7NXL5b1RpdWWdzPNmkkoy
 clear
 echo "======================="
 echo choose ngrok region
@@ -16,6 +16,6 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3389 &>/dev/null &
+#read -p "choose ngrok region: " CRP
+./ngrok tcp --region "ap" 3389 &>/dev/null &
 echo "======================="
