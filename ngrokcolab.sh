57b9e10bf5df6fbe9adae7d4918303c071b7bf0c
ucox777 <85094433+ucox777@users.noreply.github.com>
2023-01-07 00:03:08 +0700

    ee

--- ngrokcolab.sh
+++ ngrokcolab.sh
@@ -1,7 +1,7 @@
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 echo "======================="
 echo "Setting ngrok"
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken $2
 clear
