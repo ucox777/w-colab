afaf317747b99dca86394f38acaea68a2343b00c
ucox777 <85094433+ucox777@users.noreply.github.com>
2023-01-06 22:22:04 +0700

    Create ngrokcolab.sh

---
+++ ngrokcolab.sh
@@ -0,0 +1,19 @@
+echo "======================="
+echo "Setting ngrok"
+echo "======================="
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+./ngrok authtoken $2
+clear
+echo "======================="
+echo Ngrok region list
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+./ngrok tcp --region $1 3389 &>/dev/null &
+echo "======================="
