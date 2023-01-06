f3b31ae6247dd9e7f4d25b0c5fc66fad14d797a8
ucox777 <85094433+ucox777@users.noreply.github.com>
2023-01-07 03:45:52 +0700

    Update start.sh

--- xface4/start.sh
+++ xface4/start.sh
@@ -12,7 +12,8 @@ sudo apt install firefox -y > /dev/null 2>&1
 echo "===================================="
 echo "Install xface4 xface-terminal xrdp"
 echo "===================================="
-sudo apt install -y xrdp > /dev/null 2>&1                                                                                                              sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt install -y xrdp > /dev/null 2>&1                                    
+sudo apt install xfce4 -y > /dev/null 2>&1
 sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install tmux > /dev/null 2>&1
@@ -25,7 +26,11 @@ wget -q https://raw.githubusercontent.com/ucox777/w-colab/main/xface4/run.sh
 sudo chmod +x run.sh
 tmux new-session -d -s "rdp" ./run.sh
 echo XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'                                         echo "===================================="
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+
+                                 
+                                        
+echo "===================================="
 #echo "Don't close this tab RDP runs 12 hours"
 #echo "===================================="
 
