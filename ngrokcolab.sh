d8bd2ab1f6571e18df5d9b2419e61c9502d24a53
ucox777 <85094433+ucox777@users.noreply.github.com>
2023-01-06 23:59:36 +0700

    Update ngrokcolab.sh

--- ngrokcolab.sh
+++ ngrokcolab.sh
@@ -5,18 +5,4 @@ wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken $2
 clear
-echo "======================="
-echo Ngrok region list
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
 ./ngrok tcp --region $1 3389 &>/dev/null &
-echo "======================="
-echo  XRDP Address:
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "======================="
